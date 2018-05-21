692eb6127f9a22e334d5e2d6b6f3cf1fe82cfaff
Francesco Draicchio <fdraicchio@gmail.com>
2018-05-21 13:23:15 +0200

    added custom paragraph parser

--- date/DateParser.php
+++ date/DateParser.php
@@ -1,41 +1,41 @@
-<?php
+    <?php
 /*
  * Copyright (c) 2014 - Copyright holders CIRSFID and Department of
  * Computer Science and Engineering of the University of Bologna
- * 
- * Authors: 
+ *
+ * Authors:
  * Monica Palmirani – CIRSFID of the University of Bologna
  * Fabio Vitali – Department of Computer Science and Engineering of the University of Bologna
  * Luca Cervone – CIRSFID of the University of Bologna
- * 
+ *
  * Permission is hereby granted to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The Software can be used by anyone for purposes without commercial gain,
  * including scientific, individual, and charity purposes. If it is used
  * for purposes having commercial gains, an agreement with the copyright
  * holders is required. The above copyright notice and this permission
  * notice shall be included in all copies or substantial portions of the
  * Software.
- * 
+ *
  * Except as contained in this notice, the name(s) of the above copyright
  * holders and authors shall not be used in advertising or otherwise to
  * promote the sale, use or other dealings in this Software without prior
  * written authorization.
- * 
+ *
  * The end-user documentation included with the redistribution, if any,
  * must include the following acknowledgment: "This product includes
  * software developed by University of Bologna (CIRSFID and Department of
- * Computer Science and Engineering) and its authors (Monica Palmirani, 
+ * Computer Science and Engineering) and its authors (Monica Palmirani,
  * Fabio Vitali, Luca Cervone)", in the same place and form as other
  * third-party acknowledgments. Alternatively, this acknowledgment may
  * appear in the software itself, in the same form and location as other
  * such third-party acknowledgments.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -46,12 +46,12 @@
  */
 
 class DateParser {
-    
+
     public $lang;
     private $patterns = array();
     private $monthsNames = array();
     private $dates = array();
-    
+
     public function __construct($lang) {
         $this->lang = $lang;
         $this->dirName = dirname(__FILE__);
@@ -84,7 +84,7 @@ class DateParser {
                 foreach ($n["0"] as $k => $value) {
                     $offset = $n["0"][$k][1];
                     if (isset($n['date_match'][$k][0]))
-                        $match = $n["date_match"][$k][0];    
+                        $match = $n["date_match"][$k][0];
                     else $match = $n["0"][$k][0];
                     $offsetEnd = $offset+strlen($match);
 
@@ -173,18 +173,18 @@ class DateParser {
 
         $ret = array("response" => $return);
         if($jsonOutput) {
-            return json_encode($ret);    
+            return json_encode($ret);
         } else {
             return $ret;
         }
-        
+
     }
 
     private function isValidMatch($match, $start, $end) {
         $dates = array_filter($this->dates, function($date) use ($match, $start, $end)  {
             return ((strpos($date["match"], $match) !== FALSE || strpos($match, $date["match"]) !== FALSE)
                     && $date["start"] <= $start && $date["end"] >= $end );
-                    //&& $date["start"] <= $start && $date["end"] >= $end 
+                    //&& $date["start"] <= $start && $date["end"] >= $end
                     //&& !($date["start"] == $start && $date["end"] == $end));
         });
         return empty($dates);
@@ -193,12 +193,12 @@ class DateParser {
     public function loadConfiguration() {
         global $patterns, $monthsNames;
         global $day,$year,$num_month,$date_sep;
-        $vocs = array("ita" => "italian.php", 
-                      "eng" => "english.php", 
-                      "esp" => "espanol.php", 
+        $vocs = array("ita" => "italian.php",
+                      "eng" => "english.php",
+                      "esp" => "espanol.php",
                       "spa" => "espanol.php",
                       "rum" => "romanian.php",);
-        
+
         $day = "(\d){1,2}";
         $year = "(\d){4}";
         $num_month = "(\d){1,2}";
@@ -211,8 +211,8 @@ class DateParser {
         }
 
         $months = implode("|", $monthsNames);
-        $patterns = array("(day) month-name year" => "/((?P<day>$day)(\s*°)?\W?\s+)?(?P<month>$months)\W?\s+(?P<year>$year)/i", 
-                    "month-name (day) year" => "/(?P<month>$months)\W?\s+((?P<day>$day)\W?\s+)?(?P<year>$year)/i", 
+        $patterns = array("(day) month-name year" => "/((?P<day>$day)(\s*°)?\W?\s+)?(?P<month>$months)\W?\s+(?P<year>$year)/i",
+                    "month-name (day) year" => "/(?P<month>$months)\W?\s+((?P<day>$day)\W?\s+)?(?P<year>$year)/i",
                     "month-name year (day)" => "/(?P<month>$months)\W?\s+(?P<year>$year)(\W?\s+(?P<day>$day))?/i");
 
         if (isset($localpatterns)) {
