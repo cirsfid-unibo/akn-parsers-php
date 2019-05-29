5471be520fd4b199aa20218e841162debfd06994
Francesco Draicchio <fdraicchio@gmail.com>
2019-05-29 19:06:46 +0200

    added integrity check on resolution_miner

--- python-module/resolution_miner.py
+++ python-module/resolution_miner.py
@@ -4,10 +4,10 @@ import sys,re,json
 PORT_NUMBER = int(sys.argv[1]) if len(sys.argv)>1 else 8080
 
 class MinerService(BaseHTTPRequestHandler):
-	
+
 	__resolution_re = re.compile(r'^Resolution\s(\d{1,2})/(\d{4})', re.M|re.I)
 	__adopted_re = re.compile(r'^\(?Adopted\son\s\d{1,2}\s[A-Za-z]+\s(\d{4})\)?', re.M|re.I)
-	
+
 	def do_POST(self):
 		content_length = int(self.headers['Content-Length'])
 		post_data = self.rfile.read(content_length)
@@ -18,6 +18,7 @@ class MinerService(BaseHTTPRequestHandler):
 		self.wfile.write(self.__mine(post_data))
 
 	def __mine(self,raw):
+		print raw
 		starts = [] ; ends = []
 		for i in self.__resolution_re.finditer(raw):
 			starts.append({"number" : i.group(1), "year" : i.group(2), "start" : (i.start())})
@@ -32,14 +33,30 @@ class MinerService(BaseHTTPRequestHandler):
 				if i['start'] < j['end'] and i['year'] == j['year']:
 					result.append({'start' : i['start'], 'end' : j['end'], 'number' : i['number'], 'year' : i['year']})
 					break
-		return json.dumps(self.__consistency_check(result))
-		
+		return json.dumps(self.__double_check(result))
+
 	def __consistency_check(self,li):
 		for i in li:
 			if int(i['number']) != int(li.index(i)+1):
 				li.append({'consistency' : False})
 				break
 		return li
+
+	def __integrity_check(self,li):
+		try:
+			end = int(li[0]['end'])
+			for i in li[1:]:
+				if end > i['start']:
+					li.append({'integrity' : False})
+					break
+				else: end = int(i['end'])
+			return li
+		except IndexError:
+			return li
+
+	def __double_check(self,li):
+		return self.__integrity_check(
+			self.__consistency_check(li))
 
 try:
 	server = HTTPServer(('', PORT_NUMBER), MinerService)
