e44b02afe3d3cf9a3d8ec133ba7f4fe840176c78
Octavian Bujor <octavian.bujor@unibo.it>
2019-05-30 08:20:49 +0200

    Allow CORS resolution_miner

--- python-module/resolution_miner.py
+++ python-module/resolution_miner.py
@@ -14,6 +14,7 @@ class MinerService(BaseHTTPRequestHandler):
 
 		self.send_response(200)
 		self.send_header('Content-type','application/json')
+		self.send_header('Access-Control-Allow-Origin', '*')
 		self.end_headers()
 		self.wfile.write(self.__mine(post_data))
 
